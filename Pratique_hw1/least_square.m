67e42fec46fa5b67a6c97a6c973d74190f34b77a
Prateek <prateekarorav2@gmail.com>
2018-09-06 23:18:27 -0400

    regularization equation updated. Now accounts for intercept

--- Pratique_hw1/least_square.m
+++ Pratique_hw1/least_square.m
@@ -11,105 +11,105 @@ Y_2 = data_2.pts(2,:).';
 %splitting data_3 into vectors
 X_3 = data_3.pts(1,:).';
 Y_3 = data_3.pts(2,:).';
-%defining a vector of ones for convenience
-% vector_of_ones = ones(max(size(X_1)),1);
-% A = [X_1 Y_1 vector_of_ones];
-%optimization parameter
-% theta = [a;b;c];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %			Homework question 2 - Linear Regression
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%using (backslash)
+%%%%%%%using (backslash)
 format long
 %for data_1
 B = inv(X_1.'*X_1)*X_1.'*Y_1;  % can also be written as B = X_1\Y_1;
 y_cal = B*X_1;
 figure('Name','linear regression using backslash');
-subplot(2,2,1);
+subplot(3,1,1);
 scatter(X_1,Y_1);
+axis([-150 150 -150 150]);
 hold on
 plot(X_1,y_cal);
 hold off
 %for data_2
 B = X_2\Y_2;
 y_cal = B*X_2;
-subplot(2,2,2);
+subplot(3,1,2);
 scatter(X_2,Y_2);
+axis([-150 150 -150 150]);
 hold on
 plot(X_2,y_cal);
 hold off
 %for data_3
 B = X_3\Y_3;
 y_cal = B*X_3;
-subplot(2,2,3);
+subplot(3,1,3);
 scatter(X_3,Y_3);
+axis([-150 150 -150 150]);
 hold on
 plot(X_3,y_cal);
 hold off
 
-
-
-
-%using polyfit
+%%%%%%%%using polyfit
 p = polyfit(X_1,Y_1,1); 
 f = polyval(p,X_1); 
 figure('Name','using polyfit function');
-subplot(2,2,1);
+subplot(3,1,1);
+axis([-150 150 -150 150]);
 plot(X_1,Y_1,'o',X_1,f,'-');
 p = polyfit(X_2,Y_2,1); 
 f = polyval(p,X_2); 
-subplot(2,2,2);
+subplot(3,1,2);
+axis([-150 150 -150 150]);
 plot(X_2,Y_2,'o',X_2,f,'-');
 p = polyfit(X_3,Y_3,1); 
 f = polyval(p,X_3); 
-subplot(2,2,3);
+subplot(3,1,3);
+axis([-150 150 -150 150]);
 plot(X_3,Y_3,'o',X_3,f,'-');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %			Homework question 2 - Regularization
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% for data_1
-
-% lambda = [-100000 -10000 -1000 -100 -10 1 10 100 1000 10000 100000];  %created a vector to reduce manual hit and trial effort
+%%%%%%% for data_1
+% lambda = [-1000000 -100000 -10000 1000 10000 100000];  %created a vector to reduce manual hit and trial effort
 lambda = -100000;
+X_1 = [ones(size(X_1,1),1) X_1];
 % for i = 1:max(size(lambda))
-	B = inv(X_1.'*X_1 + lambda)*X_1.'*Y_1; 
-	% y_cal = B*X_1;
-	y_cal_adjust = B*X_1+10;		%added an offset of ten for fun(fits better!)
+	% disp(size(X_1));
+	% figure(i);
+	B = inv(X_1.'*X_1 + lambda*eye(2))*X_1.'*Y_1;
+	y_cal_adjust = X_1*B;						% y = mx + c
 	figure('Name',"Using Regularization");
-	% subplot(2,1,1);
-	% scatter(X_1,Y_1);
-	% hold on
-	% plot(X_1,y_cal);
-	% hold off
-	subplot(2,2,1);
-	scatter(X_1,Y_1);
-	hold on
-	plot(X_1,y_cal_adjust);
-	hold off
+	subplot(3,1,1);
+	axis([-150 150 -150 150]);
+	scatter(X_1(:,2:end),Y_1);
+	hold on;
+	plot(X_1(:,2:end),y_cal_adjust);
+	hold off;
 % end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% for data_2
-lambda = -280000;
-B = inv(X_2.'*X_2 + lambda)*X_2.'*Y_2; 
-y_cal_adjust = B*X_2+10;		%
-subplot(2,2,2);
-scatter(X_2,Y_2);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% for data_2
+	lambda = -300000;
+	X_2 = [ones(size(X_2,1),1) X_2];
 
-hold on
-plot(X_2,y_cal_adjust);
-hold off
+	B = inv(X_2.'*X_2 + lambda*eye(2))*X_2.'*Y_2; 
+	y_cal_adjust = X_2*B;		%
+	subplot(3,1,2);
+	scatter(X_2(:,2:end),Y_2);
+	axis([-150 150 -150 150]);
+	hold on
+	plot(X_2(:,2:end),y_cal_adjust);
+	hold off
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% for data_3
-lambda = -500000;
-B = inv(X_3.'*X_3 + lambda)*X_3.'*Y_3; 
-y_cal_adjust = B*X_3+10;		%
-subplot(2,2,3);
-scatter(X_3,Y_3);
-hold on
-plot(X_3,y_cal_adjust);
-hold off
+	lambda = -500000;
+	X_3 = [ones(size(X_3,1),1) X_3];
+
+	B = inv(X_3.'*X_3 + lambda*eye(2))*X_3.'*Y_3; 
+	y_cal_adjust = X_3*B;	
+	subplot(3,1,3);
+	scatter(X_3(:,2:end),Y_3);
+	axis([-150 150 -150 150]);
+	hold on
+	plot(X_3(:,2:end),y_cal_adjust);
+	hold off
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %			Homework question 2 - RANSAC
--- Pratique_hw1/plot_eigen.m
+++ Pratique_hw1/plot_eigen.m
@@ -3,16 +3,11 @@ data_1 = load('hw1_data/data1.mat');
 data_2 = load('hw1_data/data2.mat');
 data_3 = load('hw1_data/data3.mat');
 
-% corrplot(data_1.pts);
-% coef = pca(data_1.pts);
-% plot(coef); 
 %segregating data_1 into vectors
 v1 = data_1.pts(1,:);
 v2 = data_1.pts(2,:);
 covarience_matrix_1 = cov(v1,v2);
 [Evec_1, Eval_1] = eig(covarience_matrix_1)
-visualize_eigen_1 = Evec_1(:,1)*(sqrt(Eval_1(1:1)));
-visualize_eigen_2 = Evec_1(:,2)*(sqrt(Eval_1(4:4)));
 %segregating data_1 into vectors
 v3 = data_2.pts(1,:);
 v4 = data_2.pts(2,:);
@@ -29,11 +24,11 @@ figure(1);
 % subplot(1,2,1);
 scatter(v1,v2);
 hold on;
-x1 = [Evec_1(1,1),-Evec_1(1,1)]*sqrt(Eval_1(1,1));
-y1 = [Evec_1(2,1),-Evec_1(2,1)]*sqrt(Eval_1(1,1));
+x1 = [Evec_1(1,1),-Evec_1(1,1)]*sqrt(Eval_1(1,1)*2);	%scaling the eigen vectors by eigen values
+y1 = [Evec_1(2,1),-Evec_1(2,1)]*sqrt(Eval_1(1,1)*2);
 
-x2 = [Evec_1(1,2),-Evec_1(1,2)]*sqrt(Eval_1(2,2));
-y2 = [Evec_1(2,2),-Evec_1(2,2)]*sqrt(Eval_1(2,2));
+x2 = [Evec_1(1,2),-Evec_1(1,2)]*sqrt(Eval_1(2,2)*2);
+y2 = [Evec_1(2,2),-Evec_1(2,2)]*sqrt(Eval_1(2,2)*2);
 axis([-200,200,-200,200]);
 plot(x1,y1,'LineWidth',3);
 plot(x2,y2,'LineWidth',3);
@@ -46,11 +41,11 @@ figure(2);
 scatter(v3,v4);
 hold on;
 axis([-200,200,-200,200]);
-x1 = [Evec_2(1,1),-Evec_2(1,1)]*sqrt(Eval_2(1,1));
-y1 = [Evec_2(2,1),-Evec_2(2,1)]*sqrt(Eval_2(1,1));
+x1 = [Evec_2(1,1),-Evec_2(1,1)]*sqrt(Eval_2(1,1)*2);	%scaling the eigen vectors by eigen values
+y1 = [Evec_2(2,1),-Evec_2(2,1)]*sqrt(Eval_2(1,1)*2);
 
-x2 = [Evec_2(1,2),-Evec_2(1,2)]*sqrt(Eval_2(2,2));
-y2 = [Evec_2(2,2),-Evec_2(2,2)]*sqrt(Eval_2(2,2));
+x2 = [Evec_2(1,2),-Evec_2(1,2)]*sqrt(Eval_2(2,2)*2);
+y2 = [Evec_2(2,2),-Evec_2(2,2)]*sqrt(Eval_2(2,2)*2);
 plot(x1,y1,'LineWidth',3);
 plot(x2,y2,'LineWidth',3);
 hold off;
@@ -59,11 +54,11 @@ figure(3);
 scatter(v5,v6);
 hold on;
 axis([-200,200,-200,200]);
-x1 = [Evec_3(1,1),-Evec_3(1,1)]*sqrt(Eval_3(1,1));
-y1 = [Evec_3(2,1),-Evec_3(2,1)]*sqrt(Eval_3(1,1));
+x1 = [Evec_3(1,1),-Evec_3(1,1)]*sqrt(Eval_3(1,1)*2);	%scaling the eigen vectors by eigen values
+y1 = [Evec_3(2,1),-Evec_3(2,1)]*sqrt(Eval_3(1,1)*2);
 
-x2 = [Evec_3(1,2),-Evec_3(1,2)]*sqrt(Eval_3(2,2));
-y2 = [Evec_3(2,2),-Evec_3(2,2)]*sqrt(Eval_3(2,2));
+x2 = [Evec_3(1,2),-Evec_3(1,2)]*sqrt(Eval_3(2,2)*2);
+y2 = [Evec_3(2,2),-Evec_3(2,2)]*sqrt(Eval_3(2,2)*2);
 plot(x1,y1,'LineWidth',3);
 plot(x2,y2,'LineWidth',3);
 hold off;
